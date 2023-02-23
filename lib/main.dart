a076d5731a5abd4ee2371e3085d7117d32c5e78e
Arsh <arshbhatia1551@gmail.com>
2023-02-23 23:06:17 +0530

    correction in name

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Interview-Bit',
+      title: 'Scaler-App',
       home: AdminScreen(),
       debugShowCheckedModeBanner: false,
     );
